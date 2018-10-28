1cbdce2658e1e9f48d8c5fc5c9fca5c372977a5a
Simon Headley <simonheadley@conor.co.za>
2018-10-28 18:45:10 +0200

    doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+// A library of code which abstracts and encapsulates the details of connecting and configuration an AMQP connection a RabbitMQ Broker while still providing the majority of the flexibility gained by referring to the Go AMQP library directly.
+
+// Authors: Simon Headley (KrylixZA)
+
+package gorabbitmqbroker
