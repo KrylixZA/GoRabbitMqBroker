611378740a628a30dfd7db6cd3f4e45fc380fbd6
Simon Headley <simonheadley@conor.co.za>
2018-10-29 22:18:57 +0200

    updated gorabbitmqbroker doc.go

--- doc.go
+++ doc.go
@@ -3,4 +3,10 @@
 // The user will pass this configuration to the message broker which will handle the details of connecting to RabbitMQ and defining the queues and exchanges.
 // After this, the user provides the necessary interfaces to the broker and it will handle the details of subscribing and reading messages or publishing messages as per the config.
 
+// Known issues can be found [here](https://github.com/KrylixZA/GoRabbitMqBroker/issues)
+
+// This code is licensed under an MIT license.
+
+// Authors: Simon Headley (KrylixZA)
+
 package gorabbitmqbroker
